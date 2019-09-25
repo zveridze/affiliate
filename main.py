ba5f1b2c3df86ee543d7766908bbc23ed042f001
kochergini <d;Jb;5?IorX0*+|>
2019-09-25 17:58:53 +0300

    0.0.3

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from functions import do_sql
 
 DB_NAME = 'users.db'
 SQL_WHAT_TABLES = "SELECT name FROM sqlite_master WHERE type='table';"
-SQL_CREATE_TABLES = "CREATE TABLE users(id INTEGER PRIMARY KEY, email, password);"
+SQL_CREATE_TABLES = "CREATE TABLE users" \
+                    " (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," \
+                    " email TEXT UNIQUE NOT NULL," \
+                    " password TEXT NOT NULL);"
+SQL_CREATE_TESTER = "INSERT INTO users (email, password) VALUES ( 'tester', 'testerpass' );"
 
 app = Flask(__name__)
 
@@ -26,22 +30,29 @@ def register():
 @app.route('/check_login_pass', methods=['GET', 'POST'])
 def check_login_pass():
     if request.method == 'POST':
-        print(request.form)
-        return 'under construction'
-    if request.method == 'GET':
-        return 'it is a GET!'
+        print(dict(request.form))
+        return render_template('cabinet.html')
+    else:
+        return 'method must be POST!'
 
 
 @app.route('/write_to_database', methods=['GET', 'POST'])
 def write_to_database():
     if request.method == 'POST':
-        return 'POST write_to_database'
-    if request.method == 'GET':
-        return 'it is a GET!'
+        pair_key_value_from_request = dict(request.form)
+        email = pair_key_value_from_request['email']
+        password = pair_key_value_from_request['password']
+        new_query = "SELECT email FROM users where email = {0}".format(email)
+        print(do_sql(DB_NAME, new_query))
+        return render_template('cabinet.html', email=email, password=password)
+    else:
+        return 'method must be POST!'
 
 
 if __name__ == '__main__':
     # TODO проверять не только существование, но и структуру
     if not do_sql(DB_NAME, SQL_WHAT_TABLES):
         do_sql(DB_NAME, SQL_CREATE_TABLES)
+        do_sql(DB_NAME, SQL_CREATE_TESTER)
+
     app.run(debug=False)
