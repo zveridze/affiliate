a4264ce7f138cceb9ee4957f311736c0e517f27a
Дмитрий Зверев <zverev@Dmitrijs-MacBook-Pro.local>
2019-09-26 01:24:47 +0300

    Remove validators from registration form

--- app/forms.py
+++ app/forms.py
@@ -15,15 +15,16 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
 
     email = StringField('Email', validators=[DataRequired()])
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    phone = IntegerField('Phone', validators=[DataRequired()])
-    messenger_type = StringField('Messenger type', validators=[DataRequired()])
-    messenger = StringField('Messenger', validators=[DataRequired()])
+    first_name = StringField('First name')
+    last_name = StringField('Last name')
+    phone = IntegerField('Phone')
+    messenger_type = StringField('Messenger type')
+    messenger = StringField('Messenger')
     password = StringField('Password', validators=[DataRequired()])
     password2 = StringField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('Email already exist')
+            raise ValidationError('Sorry, email already exist')
