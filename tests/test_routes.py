28fe433e206f614d8aff6145d9acde43e62dc468
Дмитрий Зверев <zverev@Dmitrijs-MacBook-Pro.local>
2019-10-13 18:25:57 +0300

    Fix tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,10 +1,6 @@
 from app import create_app, db
 from config import Config
 import pytest
-import tempfile
-
-
-print(tempfile.mkstemp())
 
 
 class TestingConfig(Config):
@@ -12,26 +8,36 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
-@pytest.fixture()
-def setup():
+@pytest.fixture(scope='session')
+def app():
     app = create_app(TestingConfig)
+    return app
+
+
+@pytest.fixture()
+def setup(request, app):
     app.app_context()
     app_context = app.test_client()
     app_context.testing = True
-    db.create_all(app=app)
     return app_context
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_db(request, app):
+    with app.app_context():
+        db.create_all()
 
 
 def test_register(setup):
     rv = setup.post('/register', data=dict(email='test@mail.ru',
                                            password=123,
                                            password2=123), follow_redirects=True)
-    print(rv.data)
-    print(rv.status)
+    assert rv.status == '200 OK'
+    assert b'Registration completed successfully' in rv.data
 
 
 def test_login(setup):
     rv = setup.post('/login', data=dict(email='test@mail.ru',
                                         password=123), follow_redirects=True)
-    print(rv.data)
-    print(rv.status)
+    assert rv.status == '200 OK'
+    assert b'Logout' in rv.data
